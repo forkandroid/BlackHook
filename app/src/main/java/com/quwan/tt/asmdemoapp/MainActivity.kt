580301463649ff4e1920f318148dd78c3b5ce784
陈少平 <chenshaoping@52tt.com>
2021-10-20 11:40:30 +0800

    修改demo

--- app/src/main/java/com/quwan/tt/asmdemoapp/MainActivity.kt
+++ app/src/main/java/com/quwan/tt/asmdemoapp/MainActivity.kt
@@ -1,15 +1,8 @@
 package com.quwan.tt.asmdemoapp
 
-import android.content.Intent
-import android.content.res.Resources
-import android.os.AsyncTask
 import android.os.Build
 import android.os.Bundle
-import android.os.HandlerThread
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.annotation.RequiresApi
-
 
 class MainActivity : BaseActivity() {
 
@@ -17,25 +10,6 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val imageView: ImageView =findViewById(R.id.iv)
-        imageView.setImageDrawable(getDrawable(R.drawable.ic_launcher_background))
-        println("====>imageView:${ resources.getResourceEntryName(imageView.id)}")
-        println("====>tv:${ findViewById<TextView>(R.id.tv).toString()}")
         Thread()
-        object : AsyncTask<Void?, Void?, Void?>() {
-            override fun doInBackground(vararg params: Void?): Void? {
-                return null
-            }
-
-        }.execute()
-        Thread()
-        Thread()
-        HandlerThread("test")
-//        A().test()
-        val startIntent = Intent(this, TestService::class.java)
-        startService(startIntent)
     }
-
-
-
 }
